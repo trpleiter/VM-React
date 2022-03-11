3a65dbd4ca87f4baa4c0716301c3593cfacf9f4e
Tom Pleiter <you@example.com>
2022-03-11 17:16:09 +0100

    functionality search recipe start, still needs styling

--- src/components/reciperesult/RecipeResult.js
+++ src/components/reciperesult/RecipeResult.js
@@ -1,23 +1,39 @@
-import React from "react";
-import styles from "./RecipeResult.module.css";
-
-function RecipeResult ({picture1, name1, picture2, name2, picture3, name3, picture4, name4}) {
-    return (
-        <div className={styles.recipeResults}>
-            <section>
-                <h4>{picture1}</h4>
-                <p>{name1}</p>
-            </section>
-            <section>
-                <h4>{picture2}</h4>
-                <p>{name2}</p>
-            </section>
-            <section>
-                <h4>{picture3}</h4>
-                <p>{name3}</p>
-            </section>
-        </div>
-    )
-}
-
-export default RecipeResult
+// import React, { useState, useEffect } from "react";
+// import styles from "./RecipeResult.module.css";
+// import axios from "axios";
+//
+// function RecipeResult ({ leftover }) {
+//     const [recipes, setRecipes] = useState([]);
+//
+//     useEffect(() => {
+//         async function fetchRecipe() {
+//             try {
+//                 const result = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${leftover}&apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
+//                 console.log(result.data);
+//                 setRecipes(result.data);
+//             } catch (e) {
+//                 console.error(e);
+//             }
+//         };
+//
+//         if (leftover) {
+//             fetchRecipe();
+//         }
+//
+//     }, [leftover]);
+//
+//     return (
+//         <div className={styles.recipeResults}>
+//                 {recipes.map((infoRecipes) => {
+//                     return(
+//                         <article key={infoRecipes.id}>
+//                             <h4>{infoRecipes.title}</h4>
+//                             <img className="recipeimg" src={infoRecipes.image} alt="recipe"/>
+//                         </article>
+//                     )
+//                 })}
+//         </div>
+//     )
+// }
+//
+// export default RecipeResult
--- src/pages/voedie-stomach.js
+++ src/pages/voedie-stomach.js
@@ -9,22 +9,50 @@ import cookicon from "../assets/cook-icon.png";
 import Button from "../components/button/Button";
 import Footer from "../components/footer/Footer";
 import RecipeResult from "../components/reciperesult/RecipeResult";
+import styles from "../components/reciperesult/RecipeResult.module.css";
 
 function VoedieStomach() {
     const [query, setQuery] = useState('');
     const [leftover, setLeftover] = useState('');
-    const [recipes, setRecipes] = useState('');
+
+    const defaultRecipes = [
+        {
+            id: 640803,
+            image: "https://spoonacular.com/recipeImages/640803-312x231.jpg",
+            title: "Crispy Buttermilk Fried Chicken",
+        },
+        {
+            id: 665734,
+            image: "https://spoonacular.com/recipeImages/665734-312x231.jpg",
+            title: "Zucchini Chicken Omelette",
+        },
+        {
+            id: 649722,
+            image: "https://spoonacular.com/recipeImages/649722-312x231.jpg",
+            title: "Lemon Pepper Steak",
+        },
+        {
+            id: 652965,
+            image: "https://spoonacular.com/recipeImages/652965-312x231.jpg",
+            title: "Nasi Pudina (Mint Rice)",
+        },
+    ];
+
+    const [recipes, setRecipes] = useState(defaultRecipes);
 
     useEffect(() => {
-    async function fetchRecipe() {
-        try {
-            const result = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${leftover}&apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
-            console.log(result.data);
-            setRecipes(result.data);
-        } catch (e) {
-            console.error(e);
-        }
-    };
+        async function fetchRecipe() {
+            try {
+                const result = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${leftover}&apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
+                console.log(result.data);
+                setRecipes(result.data);
+            } catch (e) {
+                console.error(e);
+                return (
+                    <p>Something went wrong, please try again..</p>
+                )
+            }
+        };
 
         if (leftover) {
             fetchRecipe();
@@ -32,9 +60,6 @@ function VoedieStomach() {
 
     }, [leftover]);
 
-
-
-console.log(recipes);
 
         function onFormSubmit(event) {
         event.preventDefault();
@@ -97,23 +122,18 @@ console.log(recipes);
 
             <h3>Delicious recipes</h3>
             <div className="background-container">
-                <RecipeResult
-                    picture1="Picture"
-                    name1="Name"
-                    picture2="Picture"
-                    name2="Name"
-                    picture3="Picture"
-                    name3="Name"
-
-                />
-                <RecipeResult
-                    picture1="Picture"
-                    name1="Name"
-                    picture2="Picture"
-                    name2="Name"
-                    picture3="Picture"
-                    name3="Name"
-                />
+                <div className={styles.recipeResults}>
+                    {recipes.map((infoRecipes) => {
+                        return(
+                            <article className="recipeResult" key={infoRecipes.id}>
+                                <h4>{infoRecipes.title}</h4>
+                                <img className="recipeimg" src={infoRecipes.image} alt="recipe"/>
+                                <p>You are only missing {infoRecipes.missedIngredientCount} ingredient(s) to make this recipe!
+                                </p>
+                            </article>
+                        )
+                    })}
+                </div>
             </div>
             <Footer/>
         </>
