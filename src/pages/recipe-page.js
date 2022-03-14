7a90e968c9113839d8059e0e09eef0083364aca1
Tom Pleiter <you@example.com>
2022-03-14 13:53:05 +0100

    progress recipepage

--- src/pages/recipe-page.js
+++ src/pages/recipe-page.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import _ from "lodash";
 import { useParams } from "react-router-dom";
 import '../styles/recipe-page.css';
 import NavigationBar from "../components/navigationbar/Nav-bar";
@@ -9,21 +10,31 @@ import Footer from "../components/footer/Footer";
 
 function Recipepage( {} ) {
 
-
     const { id , name} = useParams();
     const [recipeDetails, setRecipeDetails] = useState([]);
     useEffect(() => {
     async function fetchIngredientById() {
         try {
             const resultRecipeDetails = await axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
-            setRecipeDetails(resultRecipeDetails.data);
+            setRecipeDetails(resultRecipeDetails.data[0].steps);
 
         } catch (e) {
             console.error(e);
         }
-    } }, []);
+    }
+    fetchIngredientById();
 
-    console.log(recipeDetails);
+    }, []);
+
+    const recipeEquipment = recipeDetails.map((equipmentForRecipe) => {
+        return equipmentForRecipe.equipment;
+    });
+
+    const recipeIngredients = recipeDetails.map((ingredientsForRecipe) =>{
+        return ingredientsForRecipe.ingredients;
+    });
+
+    console.log(recipeIngredients);
 
         return (
             <>
@@ -33,7 +44,35 @@ function Recipepage( {} ) {
                         <div className="recipe-container">
                             <InteractionIntro>
                                 <h3>Save your leftovers by making: {name}</h3>
-                                <p>we need </p>
+                                <ul>
+                                    {recipeEquipment.map((equipment) => {
+                                        return (
+                                            <li>
+                                                {equipment.name}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+
+                                <ul>
+                                    {recipeIngredients.map((ingredients) => {
+                                        return (
+                                            <li>
+                                                {ingredients.name}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+
+                                <ol>
+                                {recipeDetails.map((infoRecipes) => {
+                                    return (
+                                                <li>
+                                                {infoRecipes.step}
+                                                </li>
+                                    )
+                                })}
+                                </ol>
                             </InteractionIntro>
                         </div>
                     </main>
