87cc25a8d11cb93218d8b15c01a41d24761b1c9f
Tom Pleiter <you@example.com>
2022-03-14 17:26:55 +0100

    finalizing recipe page

--- src/pages/recipe-page.js
+++ src/pages/recipe-page.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
-import _ from "lodash";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import '../styles/recipe-page.css';
 import NavigationBar from "../components/navigationbar/Nav-bar";
 import InteractionIntro from "../components/interactionintro/Interaction-intro";
 import Footer from "../components/footer/Footer";
 
 
-function Recipepage( {} ) {
+function Recipepage({}) {
 
-    const { id , name} = useParams();
+    const {id, name} = useParams();
     const [recipeDetails, setRecipeDetails] = useState([]);
     useEffect(() => {
-    async function fetchIngredientById() {
-        try {
-            const resultRecipeDetails = await axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
-            setRecipeDetails(resultRecipeDetails.data[0].steps);
+        async function fetchIngredientById() {
+            try {
+                const resultRecipeDetails = await axios.get(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=f7b5d72783cd4b168e57cc54e500f7ed`);
+                setRecipeDetails(resultRecipeDetails.data[0].steps);
 
-        } catch (e) {
-            console.error(e);
+            } catch (e) {
+                console.error(e);
+            }
         }
-    }
-    fetchIngredientById();
+
+        fetchIngredientById();
 
     }, []);
 
@@ -30,59 +30,71 @@ function Recipepage( {} ) {
         return equipmentForRecipe.equipment;
     });
 
-    const recipeIngredients = recipeDetails.map((ingredientsForRecipe) =>{
+    let equipmentNames = recipeEquipment.flatMap(equipment => equipment);
+
+    const recipeIngredients = recipeDetails.map((ingredientsForRecipe) => {
         return ingredientsForRecipe.ingredients;
     });
 
-    console.log(recipeIngredients);
+    let ingredientNames = recipeIngredients.flatMap(ingredients => ingredients);
 
-        return (
-            <>
-                <NavigationBar/>
-                <div className="page-container-recipe">
-                    <main className="recipe-main">
-                        <div className="recipe-container">
-                            <InteractionIntro>
-                                <h3>Save your leftovers by making: {name}</h3>
-                                <ul>
-                                    {recipeEquipment.map((equipment) => {
-                                        return (
-                                            <li>
-                                                {equipment.name}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
+    console.log(ingredientNames);
 
-                                <ul>
-                                    {recipeIngredients.map((ingredients) => {
-                                        return (
-                                            <li>
-                                                {ingredients.name}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-
-                                <ol>
-                                {recipeDetails.map((infoRecipes) => {
+    return (
+        <>
+            <NavigationBar/>
+            <div className="page-container-recipe">
+                <main className="recipe-main">
+                    <div className="recipe-container">
+                        <InteractionIntro>
+                            <article className="recipe-instructions">
+                            <h3>Save your leftovers by making: {name}</h3>
+                            <p>In order to start making this delicious VoedieMeal, you will need the following
+                                equipment:</p>
+                            <ul>
+                                {equipmentNames.map((equipment) => {
                                     return (
-                                                <li>
-                                                {infoRecipes.step}
-                                                </li>
+                                        <li>
+                                            {equipment.name}
+                                        </li>
                                     )
                                 })}
-                                </ol>
-                            </InteractionIntro>
-                        </div>
-                    </main>
-                </div>
-                <Footer
-                    fillpage="lower-footer"
-                />
-            </>
-        )
-    }
+                            </ul>
+                            <p>Alright, now that we mentioned all the equipment, we will follow up with all the
+                                ingredients that are needed:</p>
+                            <ul>
+                                {ingredientNames.map((ingredients) => {
+                                    return (
+                                        <li>
+                                            {ingredients.name}
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                            <p>Are you ready to start cooking? Follow our instructions in below order and everything
+                                will be fine!</p>
+                            <ol>
+                                {recipeDetails.map((infoRecipes) => {
+                                    return (
+                                        <li>
+                                            {infoRecipes.step}
+                                        </li>
+                                    )
+                                })}
+                            </ol>
+                            <p>It is finally time to enjoy your VoedieMeal cozy and unabashedly! We hope to catch you
+                                again!</p>
+                            </article>
+                        </InteractionIntro>
+                    </div>
+                </main>
+            </div>
+            <Footer
+                fillpage="lower-footer"
+            />
+        </>
+    )
+}
 
 
 export default Recipepage;
